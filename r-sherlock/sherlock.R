be527e1ae40cf0ea84d14dc13fc80dc46da04666
David Stark <david.stark.mailbox@gmail.com>
2016-01-17 12:02:32 +0100

    Add geo data

--- r-sherlock/sherlock.R
+++ r-sherlock/sherlock.R
@@ -8,20 +8,28 @@ rm(list=ls())
 library(jsonlite)
 library(dplyr)
 
+setwd("/home/david/Data/Sherlock/r-sherlock")
+
+source("geoip.R")
+
+geoobj <- freegeoip('184.26.100.110')
+#geoobj$ip
+#class(geoobj)
+
 # load JSON file into a data.frame
-json_file <- stream_in(file("/home/david/Data/r-workspace/sherlock/events-mssi.txt"))   # JSON (RFC 4627)
-class(json_file)
-summary(json_file)
-View(json_file)
+#json_file <- stream_in(file("events-mssi.txt"))   # JSON (RFC 4627)
+#class(json_file)
+#summary(json_file)
+#View(json_file)
 
 # load client data into a data.frame
-client_data <- read.csv(file="/home/david/Data/r-workspace/sherlock/client_data.txt", header=FALSE, sep=",", stringsAsFactors=FALSE)
+client_data <- read.csv(file="client_data.txt", header=FALSE, sep=",", stringsAsFactors=FALSE)
 colnames(client_data) <- c("MAC", "TIMESTAMP", "PROTOCOL", "IP_SOURCE", "PORT_SOURCE", "host.ip", "PORT_TARGET", "STATUS", "SERVICE")
 class(client_data)
 #View(client_data)
 
 # load JSON file with HOST.IP field into a data.frame
-threat_feed <- stream_in(file("/home/david/Data/r-workspace/sherlock/threat_feed.txt"))   # JSON (RFC 4627)
+threat_feed <- stream_in(file("threat_feed.txt"))   # JSON (RFC 4627)
 colnames(threat_feed)
 class(threat_feed)
 summary(threat_feed)
@@ -34,6 +42,9 @@ f_data <- flatten(threat_feed)
 
 # do left join on client_data and f_data
 t_join <- inner_join(client_data, f_data, by = "host.ip")
-
+class(t_join)
 # view report
 View(t_join)
+
+geo_data <- lapply(t_join$host.ip, freegeoip)
+View(geo_data)
